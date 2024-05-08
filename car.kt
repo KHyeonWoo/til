f91a6e7d0671f48efc20ec46fc5a0cd7d2e8703a
K-jabchae <167736533+KHyeonWoo@users.noreply.github.com>
2024-05-08 16:37:58 +0900

    Update car.kt
    main 추가

--- car.kt
+++ car.kt
@@ -1,54 +1,56 @@
+fun main() {
+    val myCar = Car("K5", 50, 30, Color.BLACK)
+    myCar.printinfo()
 
-enum class Color{
+    while (myCar.speed < 70) {
+        myCar.increaseSpeed()
+    }
+    myCar.printinfo()
+
+    myCar.addGas(70)
+    myCar.printinfo()
+
+
+    for (color in Color.values()) {
+        myCar.changeColor(color)
+        myCar.printinfo()
+        println()
+    }
+}
+
+
+enum class Color {
     BLACK,
     GRAY,
     WHITE
 }
 
-class Car (
+class Car(
     val model: String,
     var speed: Int,
     var gas: Int,
     var color: Color,
-    ){
-    fun getSpeeds(): Int{
+) {
+    fun getSpeeds(): Int {
         return this.speed
     }
-    
+
     fun increaseSpeed() {
         this.speed++
     }
-    
-    fun addGas(newGas: Int): Unit{
+
+    fun addGas(newGas: Int): Unit {
         this.gas += newGas
     }
-    
-    fun changeColor(newColor: Color){
+
+    fun changeColor(newColor: Color) {
         this.color = newColor
     }
-    
-    fun printinfo(){
+
+    fun printinfo() {
         println("model: $model")
         println("speed: $speed")
         println("gas: $gas")
         println("color: $color")
     }
-}
-
-val myCar = Car("K5", 50, 30, Color.BLACK)
-myCar.printinfo()
-
-while (myCar.speed < 70) {
-    myCar.increaseSpeed()
-}
-myCar.printinfo()
-
-myCar.addGas(70)
-myCar.printinfo()
-
-
-for(color in Color.values()) {
-    myCar.changeColor(color)
-    myCar.printinfo()
-    println()
 }
